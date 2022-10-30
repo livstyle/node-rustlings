3d4d0e701fcc32c17dd6a8aa4f159b483557b4d7
姜坤 <gains9@sina.com>
2022-10-30 15:05:35 +0800

    commander

--- bin/rustlings.js
+++ bin/rustlings.js
@@ -1,5 +1,60 @@
+const {program, Command} = require('commander');
+
+const rust = require('../');
+
 const bootstrap = () => {
-    console.log('nrustlings==========================');
-  };
+  program
+    .version(
+      require('../package.json').version,
+      '-v, --version',
+      'Output the current version.',
+    )
+    .usage('<command> [options]')
+    .helpOption('-h, --help', 'Output usage information.');
+
+    if (!process.argv.slice(2).length) {
+      program.outputHelp();
+    }
+
+  program.command('list')
+    .action(() => {
+      rust.list();
+    });
+
+  program.command('watch')
+    .action(() => {
+      rust.watch();
+    });  
+
+  program.command('verify')
+    .action(() => {
+      rust.verify();
+    }); 
+
+  program.command('reset')
+    .argument('<name>')
+    .action((name) => {
+      rust.reset(name);
+    }); 
+
+  program.command('run')
+    .argument('<name>')
+    .action((name) => {
+      rust.run(name);
+    }); 
+
+  program.command('hint')
+    .argument('<name>')
+    .action((name) => {
+      rust.hint(name);
+    }); 
+
+  program.command('lsp')
+    .action(() => {
+      rust.lsp();
+    }); 
+
+  program.parse(process.argv);
+};
   
-  bootstrap();
+bootstrap();
