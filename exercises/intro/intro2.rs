3b1ba85d4d5e966f308ca117fb20c4122f5e7a04
姜坤 <gains9@sina.com>
2022-11-06 12:34:18 +0800

    fix

---
+++ exercises/intro/intro2.rs
@@ -0,0 +1,9 @@
+// intro2.rs
+// Make the code print a greeting to the world.
+// Execute `rustlings hint intro2` or use the `hint` watch subcommand for a hint.
+
+
+fn main() {
+    let w = "world";
+    println!("Hello {}!", w);
+}
